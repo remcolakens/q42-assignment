82330a50f5853fdc00839a4c9b897b3b2c96e5c4
Remco Lakens <remco.lakens@gmail.com>
2019-10-21 22:59:07 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Fibonacci />
+  </div>
+</template>
+
+<script>
+import Fibonacci from './components/Fibonacci.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Fibonacci
+  }
+}
+</script>
+
+<style lang="scss">
+    @import "~sanitize.css";
+
+    html {
+        font-size: 0.625rem;
+    }
+
+    body {
+        font-size: 1.6rem;
+        line-height: 1.625;
+        font-family: Helvetica, Arial, sans-serif;
+        -webkit-text-size-adjust: 100%;
+        -webkit-font-smoothing: antialiased;
+    }
+</style>
